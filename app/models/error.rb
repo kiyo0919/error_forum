1d9a1b6a1024c5ea8ed74b02095427d2b8f04d55
kiyo0919 <kakinoki0919@gmail.com>
2022-04-21 01:12:12 +0900

    [追加]いいねの実装、レイアウト

--- app/models/error.rb
+++ app/models/error.rb
@@ -12,7 +12,11 @@ class Error < ApplicationRecord
     validates :detail
     validates :solution
   end
+
   enum learning_phase: { basic_knowledge: 0, team_development: 1, advance_task: 2, advance_learning: 3,
     port_folio: 4, ai_culture: 5, cloud_culture: 6, challenge: 7, development_skill_up: 8 }
 
+  def favorited_by?(mentor)
+    favorites.where(mentor_id: mentor.id).exists?
+  end
 end
